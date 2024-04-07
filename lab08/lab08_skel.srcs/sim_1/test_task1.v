9839278a1fefd611960c5459e7765d2a5726b2ed
George Grosu <george.grosu@arggo.com>
2024-04-07 08:59:50 +0300

    Updates lab08 skeleton

---
+++ lab08/lab08_skel.srcs/sim_1/test_task1.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+// ACS
+// Computer Organization and Architecture
+//  
+// Module Name:  		test_task1
+// Project Name:		Laborator 8
+// Target Devices: 		Digilent Artix 7
+//////////////////////////////////////////////////////////////////////////////////
+
+module test_task1;
+    // Inputs
+    reg[3:0] l_r_dividend;
+    reg[3:0] l_r_divisor;
+
+    // Outputs
+    wire[3:0] l_w_quotient;
+    wire[3:0] l_w_remainder;
+    
+    // Local variables for loop
+    integer i,j;
+
+    // UUT initialization
+    task1 l_m_task1(
+        .o_w_quotient(l_w_quotient),
+        .o_w_remainder(l_w_remainder),
+        .i_w_dividend(l_r_dividend),
+        .i_w_divisor(l_r_divisor)
+    );
+    
+    // Simulation tests
+    initial begin
+        // wave files
+        // $dumpfile("test.vcd");
+        // dumpp all variables
+        // $dumpvars;
+        // Monitor varibles changes in values
+        $monitor(
+            "Time = %0t, ", $time,
+            "l_w_quotient=%0d, ", l_w_quotient,
+            "l_w_remainder=%0d, ", l_w_remainder,
+            "l_r_dividend=%0d, ", l_r_dividend,
+            "l_r_divisor=%0d, ", l_r_divisor
+        );
+
+        #10;
+        for(i = 0; i < 16; i = i + 1)
+        begin
+            l_r_dividend = i;
+            for(j = 0; j < 16; j = j + 1)
+            begin
+                l_r_divisor = j;
+                #10;
+            end
+        end
+
+        // Finish the simulation
+        $finish;
+    end
+endmodule
---
+++ lab08/lab08_skel.srcs/sim_1/test_task3.v
@@ -0,0 +1,79 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+// ACS
+// Computer Organization and Architecture
+//  
+// Module Name:  		test_task3
+// Project Name:		Laborator 8
+// Target Devices: 		Digilent Artix 7
+//////////////////////////////////////////////////////////////////////////////////
+
+module test_task3;
+    // Inputs
+    reg[3:0] l_r_a;
+    reg[3:0] l_r_b;
+    reg[2:0] l_r_sel;
+    reg l_r_clk;
+    reg l_r_reset;
+
+    // Outputs
+    wire [7:0] l_w_out;
+
+    // Local variables for loop
+    integer i,j,k;
+
+    // UUT initialization
+    task3 #(.BOUNCE(5'd2)) l_m_task3(
+        .o_w_out(l_w_out),
+        .i_w_a(l_r_a),
+        .i_w_b(l_r_b),
+        .i_w_op_sel(l_r_sel),
+        .i_w_clk(l_r_clk),
+        .i_w_reset(l_r_reset)
+    );
+
+    // CLK simulation
+    always #5 l_r_clk = ~l_r_clk;
+
+    //Simulation tests
+    initial begin
+        // wave files
+        // $dumpfile("test.vcd");
+        // dumpp all variables
+        // $dumpvars;
+        // Monitor varibles changes in values
+        $monitor(
+            "Time = %0t, ", $time,
+            "l_w_out=%0d, ", l_w_out,
+            "l_r_a=%0d, ", l_r_a,
+            "l_r_b=%0d, ", l_r_b,
+            "l_r_sel=%0d, ", l_r_sel,
+            "l_r_reset=%0d, ", l_r_reset
+            );
+
+        l_r_clk = 0;
+        l_r_reset = 1;
+        l_r_in = 0;
+        #10;
+        l_r_reset = 0;
+
+        #10;
+        for(i = 0; i < 16; i = i + 1)
+        begin
+            l_r_a = i;
+            for(j = 0; j < 16; j = j + 1)
+            begin
+                l_r_b = j;
+                for(k = 0; k < 8; k = k + 1)
+                begin
+                    l_r_sel = k;
+                    #10;
+                end
+            end
+        end
+
+        // Finish the simulation
+        $finish;
+    end
+endmodule
