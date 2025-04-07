1b2e2aca2d7abb210dd6016174b781eeb4c43527
andreisophie <andreisophie@yahoo.com>
2025-04-07 19:53:16 +0300

    Add skel for new exercises

--- lab06/lab06.srcs/sources_1/imports/skel/task4.v
+++ lab06/lab06.srcs/sources_1/imports/skel/task4.v
@@ -1,57 +1,60 @@
 `timescale 1ns / 1ps
-`include "defines.vh"
 
 //////////////////////////////////////////////////////////////////////////////////
 // ACS
-// Computer Design 1
+// Computer Organization and Architecture
 //  
-// Module Name:  		ordinator_8bit
+// Module Name:  		task4
 // Project Name:		Laborator 6
-// Target Devices: 		Digilent Nexys 3
+// Target Devices: 		Digilent Nexys 7
 //////////////////////////////////////////////////////////////////////////////////
 
-module task4(
-		output wire o_w_digit_point,
-		output reg [7:0] o_r_progress,
-		output wire [0:6] o_w_segment,
-		output wire [3:0] o_w_digit,
-		input wire [7:0] i_w_operand,
-		input wire i_w_add_button,
-		input wire i_w_sub_button,
-		input wire i_w_op_button,
-		input wire i_w_reset,
-		input wire i_w_clk
+module task4
+#(
+    parameter BOUNCE    = 5'd6, // FPGA
+    parameter DELAY_BIT = 5'd6  // FPGA
+)
+(
+    output wire [7:0] o_w_AN,
+    output wire       o_w_CA,
+    output wire       o_w_CB,
+    output wire       o_w_CC,
+    output wire       o_w_CD,
+    output wire       o_w_CE,
+    output wire       o_w_CF,
+    output wire       o_w_CG,
+    output wire       o_w_DP,
+    output wire [3:0] o_w_a,
+    output wire [3:0] o_w_b,
+    output wire [2:0] o_w_op_sel,
+    input  wire [3:0] i_w_a,
+    input  wire [3:0] i_w_b,
+    input  wire [2:0] i_w_op_sel,
+    input  wire       i_w_clk,
+    input  wire       i_w_reset
+);
+    wire [7:0] l_w_out;
+    assign o_w_a = i_w_a;
+    assign o_w_b = i_w_b;
+    assign o_w_op_sel = i_w_op_sel;
+    assign o_w_AN[7:4] = 4'b1111;
+
+    // UAL module instance
+    task2 l_m_task2(
+        .o_w_result(l_w_out),
+        .i_w_a(i_w_a),
+        .i_w_b(i_w_b),
+        .i_w_op_sel(i_w_op_sel)
     );
 
-	localparam STATE_INITIAL = 0;
-	localparam STATE_OP1_READ = 1;
-	localparam STATE_OP2_READ = 2;
-	localparam STATE_RESULT_SELECT = 3;
-	
-	wire l_w_add_btn_debounced, l_w_sub_btn_debounced, l_w_op_btn_debounced;
-	
-	reg [9:0] l_r_message;
-	// TODO 4.1: Declare parameters here
-	
-	// TODO 4.2: Instantiate adder, subtractor and other useful modules 
-	
-	
-	display_7_segment_driver l_m_display(
-		.o_r_segment(o_w_segment),
-		.o_r_digit_point(o_w_digit_point),
-		.o_r_digit(o_w_digit),
-		.i_w_message(l_r_message),
-		.i_w_reset(i_w_reset),
-		.i_w_clk(i_w_clk)
-	);
-	
-	always @(posedge i_w_clk) begin
-		// TODO 4.3: Implement state change based on clock
-	end
-	
-	always @(*) begin
-		// TODO 4.4: Implement state change logic
-	end
-	
-
+    // 7LED display for showing the output in decimal
+    display_7_segment_driver
+        l_m_display7LED(
+            .o_r_digit_point(o_w_DP),
+            .o_r_segment({o_w_CA, o_w_CB, o_w_CC, o_w_CD, o_w_CE, o_w_CF, o_w_CG}),
+            .o_r_digit(o_w_AN[3:0]),
+            .i_w_message({2'b00, l_w_out}),
+            .i_w_clk(i_w_clk),
+            .i_w_reset(i_w_reset)
+        );
 endmodule
