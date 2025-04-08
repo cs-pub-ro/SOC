a6a60eec26bb7c9ecfbbfe859c06153625eb089c
Ștefan-Andrei Mărunțiș <94161930+andreisophie@users.noreply.github.com>
2025-04-08 16:46:14 +0300

    Add skel for new exercises (#10)

--- lab06/lab06.srcs/sources_1/imports/skel/task4.v
+++ lab06/lab06.srcs/sources_1/imports/skel/task4.v
@@ -1,57 +1,41 @@
 `timescale 1ns / 1ps
-`include "defines.vh"
 
 //////////////////////////////////////////////////////////////////////////////////
 // ACS
-// Computer Design 1
+// Computer Organization and Architecture
 //  
-// Module Name:  		ordinator_8bit
+// Module Name:  		task4
 // Project Name:		Laborator 6
-// Target Devices: 		Digilent Nexys 3
+// Target Devices: 		Digilent Nexys 7
 //////////////////////////////////////////////////////////////////////////////////
 
 module task4(
-		output wire o_w_digit_point,
-		output reg [7:0] o_r_progress,
-		output wire [0:6] o_w_segment,
-		output wire [3:0] o_w_digit,
-		input wire [7:0] i_w_operand,
-		input wire i_w_add_button,
-		input wire i_w_sub_button,
-		input wire i_w_op_button,
-		input wire i_w_reset,
-		input wire i_w_clk
-    );
+    output wire [7:0] o_w_result,
+    input  wire [7:0] i_w_a,
+    input  wire [7:0] i_w_b,
+    input  wire [2:0] i_w_op_sel
+);
+    // TODO 1: Instantiate task2 and task3 for ADD and SUB
 
-	localparam STATE_INITIAL = 0;
-	localparam STATE_OP1_READ = 1;
-	localparam STATE_OP2_READ = 2;
-	localparam STATE_RESULT_SELECT = 3;
-	
-	wire l_w_add_btn_debounced, l_w_sub_btn_debounced, l_w_op_btn_debounced;
-	
-	reg [9:0] l_r_message;
-	// TODO 4.1: Declare parameters here
-	
-	// TODO 4.2: Instantiate adder, subtractor and other useful modules 
-	
-	
-	display_7_segment_driver l_m_display(
-		.o_r_segment(o_w_segment),
-		.o_r_digit_point(o_w_digit_point),
-		.o_r_digit(o_w_digit),
-		.i_w_message(l_r_message),
-		.i_w_reset(i_w_reset),
-		.i_w_clk(i_w_clk)
-	);
-	
-	always @(posedge i_w_clk) begin
-		// TODO 4.3: Implement state change based on clock
-	end
-	
-	always @(*) begin
-		// TODO 4.4: Implement state change logic
-	end
-	
+    // TODO 2: Define a result reg and implement the logic to run the operation from op_sel on a and b
+    reg[7:0] l_r_result;
+  
+    always @(*) begin
+        case(i_w_op_sel)
+            // AND
+            3'd0: l_r_result = 0;
+            // XOR
+            3'd1: l_r_result = 0;
+            // OR
+            3'd2: l_r_result = 0;
+            // ADD: use task2
+            3'd3: l_r_result = 0;
+            // SUB: use task3
+            3'd4: l_r_result = 0;
+            default: l_r_result = 8'd0;
+        endcase
+    end
 
+    // TODO 3: Assign o_w_result to result
+    assign o_w_result = 0;
 endmodule
---
+++ lab06/lab06.srcs/sources_1/imports/skel/task4_test.v
@@ -0,0 +1,111 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+// ACS
+// Computer Organization and Architecture
+//  
+// Module Name:  		task4_test
+// Project Name:		Laborator 6
+// Target Devices: 		Digilent Nexys 7
+//////////////////////////////////////////////////////////////////////////////////
+
+module task4_test;
+    // Inputs
+    reg[7:0] l_r_a;
+    reg[7:0] l_r_b;
+    reg[2:0] l_r_sel;
+    reg l_r_clk;
+    reg l_r_reset;
+
+    // Outputs
+    wire [7:0] l_w_out;
+
+    // Local variables for loop
+    integer i;
+
+    // UUT initialization
+    task4 #(.BOUNCE(5'd2)) l_m_task4(
+        .o_w_out(l_w_out),
+        .i_w_a(l_r_a),
+        .i_w_b(l_r_b),
+        .i_w_op_sel(l_r_sel),
+        .i_w_clk(l_r_clk),
+        .i_w_reset(l_r_reset)
+    );
+
+    // CLK simulation
+    always #5 l_r_clk = ~l_r_clk;
+
+    //Simulation tests
+    initial begin
+        // wave files
+        // $dumpfile("test.vcd");
+        // dumpp all variables
+        // $dumpvars;
+        // Monitor varibles changes in values
+        $monitor(
+            "Time = %0t, ", $time,
+            "l_w_out=%0d, ", l_w_out,
+            "l_r_a=%0d, ", l_r_a,
+            "l_r_b=%0d, ", l_r_b,
+            "l_r_sel=%0d, ", l_r_sel,
+            "l_r_reset=%0d, ", l_r_reset
+            );
+
+        l_r_clk = 0;
+        l_r_reset = 1;
+        #10;
+        l_r_reset = 0;
+
+        // Initialize Inputs
+		l_r_a = 30;
+		l_r_b = 5;
+        
+        for (i = 0; i < 5; i = i + 1) begin
+            l_r_sel = i;
+            #100;
+        end
+		  
+		  
+		l_r_a = 100;
+		l_r_b = 37;
+		for (i = 0; i < 5; i = i + 1) begin
+            l_r_sel = i;
+            #100;
+        end
+		
+		#100;
+		
+		l_r_a = 93;
+		l_r_b = 28;
+		for (i = 0; i < 5; i = i + 1) begin
+            l_r_sel = i;
+            #100;
+        end
+		
+		#100;
+		
+		l_r_a = 56;
+		l_r_b = 23;
+		
+		for (i = 0; i < 5; i = i + 1) begin
+            l_r_sel = i;
+            #100;
+        end
+		
+		#100;
+		
+		l_r_a = 127;
+		l_r_b = 14;
+		
+		for (i = 0; i < 5; i = i + 1) begin
+            l_r_sel = i;
+            #100;
+        end
+		
+		#100;
+
+        // Finish the simulation
+        $finish;
+    end
+endmodule
